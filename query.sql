3ebeb7a48aa7a91ea2cdccac14e51be768776c75
truthprice <truthop@gmail.com>
2022-07-31 17:45:07 -0400

    first couple of queries

---
+++ query.sql
@@ -0,0 +1,8 @@
+-- SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+-- FROM employees
+-- INNER JOIN salaries ON
+-- employees.emp_no=salaries.emp_no;
+
+-- SELECT employees.first_name, employees.last_name, employees.hire_date
+-- FROM employees
+-- WHERE hire_date BETWEEN '1985-12-31' AND '1987-01-01';
